7f4c317a37ee4fb46400d75871fcff8478f9e510
Nathan Magdalera <nathanmagdalera@gmail.com>
2017-03-06 19:07:56 -0800

    Add spotify API ata

--- ui.R
+++ ui.R
@@ -12,17 +12,39 @@ library(ggplot2)
 
 
 key.jambase <- "27ye9d7m5mpepbejcxzme6pd"
-artist.name <- "chance the rapper" # Need to get this from Spotify 
+spotify.jambase <- "79db19b5259746888cc2eb93fdbbdd25"
+
+base.uri.spotify <- "https://api.spotify.com"
+search.spotify <- "/v1/search"
+uri.spotify <- paste0(base.uri.spotify, search.spotify)
+query.spotify <- list(type = "artist", q = "Kanye West") # q will have to be an interactive ariable # Obtained from Jambase API
+response.spotify <- GET(uri.spotify, query = query.spotify)
+body.spotify <- content(response.spotify, "text")
+data.spotify <- fromJSON(body.spotify)
+results.spotify <- data.spotify$artists$items$id[[1]]
+
+artist.spotify <- "/v1/artists/"
+uri.artist.spotify <- paste0(base.uri.spotify, artist.spotify, results.spotify)
+response.artist.spotify <- GET(uri.artist.spotify)
+body.artist.spotify <- content(response.artist.spotify, "text")
+data.artist.spotify <- fromJSON(body.artist.spotify)
+
+
+
+
+artist.name <- "chance the rapper" 
 base.uri.jambase <- "http://api.jambase.com"
 resource.artist.jambase <- "/artists"
-uri.artist.jambase <- paste0(base.uri.jambase, resource.artist.jambase)
-query.artist.jambase <- list(name = artist.name, api_key = key.jambase, o = "json")
-response.artist.jambase <- GET(uri.artist.jambase, query = query.artist.jambase)
-body.artist.jambase <- content(response.artist.jambase, "text")
-data.artist.jambase <- fromJSON(body.artist.jambase)
-results.artist.jambase <- data.artist.jambase$Artists
-results.artist.id.jambase <- results.artist.jambase$Id
+# Only if we want specific artist
+# uri.artist.jambase <- paste0(base.uri.jambase, resource.artist.jambase)
+# query.artist.jambase <- list(name = artist.name, api_key = key.jambase, o = "json")
+# response.artist.jambase <- GET(uri.artist.jambase, query = query.artist.jambase)
+# body.artist.jambase <- content(response.artist.jambase, "text")
+# data.artist.jambase <- fromJSON(body.artist.jambase)
+# results.artist.jambase <- data.artist.jambase$Artists
+# results.artist.id.jambase <- results.artist.jambase$Id
 
+# Grabs information about event
 resource.venue.jambase <- "/events"
 uri.venue.jambase <- paste0(base.uri.jambase, resource.venue.jambase)
 # query.venue.jambase <- list(artistID = results.artist.id.jambase, api_key = key.jambase, o = "json")
@@ -35,3 +57,4 @@ relevant.results.venue.jambase <- results.venue.jambase$Venue
 date.venue.jambase <- results.venue.jambase$Date
 relevant.results.venue.jambase <- mutate(relevant.results.venue.jambase, date = date.venue.jambase)
 us.results.venue.jambase <- relevant.results.venue.jambase %>% filter(Country == "US")
+
