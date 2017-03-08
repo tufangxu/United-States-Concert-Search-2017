5e31b72d6d1fb462541767ee64b57df0e14350ae
Zegang Cheng <czg1997@uw.edu>
2017-03-07 18:13:45 -0800

    style

--- ui.R
+++ ui.R
@@ -5,24 +5,28 @@ ui <- bootstrapPage(
   
   includeCSS("Leaflet.markercluster-1.0.3/dist/MarkerCluster.css"),
   includeCSS("Leaflet.markercluster-1.0.3/dist/MarkerCluster.Default.css"),
+  includeCSS("style.css"),
   includeScript("Leaflet.markercluster-1.0.3/dist/leaflet.markercluster.js"),
   
+  leafletOutput("map", width = "100%", height = "100%"),
   
-  selectInput(
-    'map.style',
-    "Choose your map style:",
-    list(
-      NASAGIBS.ViirsEarthAtNight2012 = providers$NASAGIBS.ViirsEarthAtNight2012,
-      Esri.WorldGrayCanvas = providers$Esri.WorldGrayCanvas,
-      CartoDB.Positron = providers$CartoDB.Positron,
-      Hydda.Full = providers$Hydda.Full,
-      Esri.WorldImagery = providers$Esri.WorldImagery
-    ),
-    selected = providers$Esri.WorldGrayCanvas
-  ),
-  
-  leafletOutput("map", width = "100%", height = "100%")
-  
+  absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                draggable = F, top = 0, left = "auto", right = "auto", bottom = "auto",
+                width = "auto", height = "auto", 
+                
+                selectInput(
+                  'map.style',
+                  "Choose your map style:",
+                  list(
+                    NASAGIBS.ViirsEarthAtNight2012 = providers$NASAGIBS.ViirsEarthAtNight2012,
+                    Esri.WorldGrayCanvas = providers$Esri.WorldGrayCanvas,
+                    CartoDB.Positron = providers$CartoDB.Positron,
+                    Hydda.Full = providers$Hydda.Full,
+                    Esri.WorldImagery = providers$Esri.WorldImagery
+                  ),
+                  selected = providers$Esri.WorldGrayCanvas
+                )
+  )
   
 )
 
