057b34d97f3387de24f483e3cfda68a575ba3ba6
Zegang Cheng <czg1997@uw.edu>
2017-03-07 17:54:17 -0800

    map

--- server.R
+++ server.R
@@ -3,12 +3,13 @@ server <- function(input, output) {
   
   output$map <- renderLeaflet({
 
-    concert.places <- as.data.frame(list(long = c(-40, - 50, - 70, - 90) , lat = c(30, 10, 40, 50)))
+    concert.places <- as.data.frame(list(long = c(-70, -90) , lat = c(40, 50)))
     concert.places <- insertRow(concert.places, getCurrentLocation(), 1)
-    leaflet(data = concert.places) %>%
-      setView(lng = -100, lat = 35, zoom = 4) %>% 
-      addMarkers(~long, ~lat, label = ~"123") %>% 
-      addPolylines(~long, ~lat) %>% 
-      addProviderTiles(input$map.style)
+    leaflet(data = concert.places, options = leafletOptions(minZoom = 2)) %>%
+      setView(lng = -100, lat = 37, zoom = 5) %>% 
+      setMaxBounds(-180, -180, 180, 180) %>% 
+      addPolylines(~long, ~lat, weight = 1, opacity = 0.2) %>% 
+      addProviderTiles(input$map.style) %>% 
+      addMarkers(~long, ~lat, clusterOptions = markerClusterOptions())
   })
 }
--- ui.R
+++ ui.R
@@ -3,6 +3,11 @@ ui <- bootstrapPage(
   
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   
+  includeCSS("Leaflet.markercluster-1.0.3/dist/MarkerCluster.css"),
+  includeCSS("Leaflet.markercluster-1.0.3/dist/MarkerCluster.Default.css"),
+  includeScript("Leaflet.markercluster-1.0.3/dist/leaflet.markercluster.js"),
+  
+  
   selectInput(
     'map.style',
     "Choose your map style:",
